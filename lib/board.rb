1e39998c6816ece2d2bc1011d79f2abf5e036d11
Kelly Bard <kellyibard@gmail.com>
2019-12-05 01:24:45 -0700

    Changed methods in board

--- lib/board.rb
+++ lib/board.rb
@@ -32,58 +32,95 @@ class Board
     @cells.has_key?(coordinate)
   end
 
+  def all_valid?
+    @placement_coordinates.all? do |coordinate|
+      valid_coordinate?(coordinate)
+    end
+  end
+
   def valid_placement?(ship, coordinates = [])
       @placement_coordinates = coordinates
       @rows = []
       @columns = []
     return false if coordinates.length != ship.length
 
-      @placement_coordinates.all? do |coordinate|
-        valid_coordinate?(coordinate)
-      end
+      all_valid?
+      # no_diagonals
       consecutive_method
-      no_diagonals
+
   end
 
   def consecutive_method
     @placement_coordinates.each do |coordinate|
         @columns << coordinate.slice(1)
         @rows << coordinate.slice(0)
+        # binding.pry
     end
 
-    columns_sorted = @columns.sort
+    rows_sorted = @rows.sort.uniq
+      a = rows_sorted[0]
+      b = rows_sorted[-1]
+    rows_range_array = (a..b).to_a
+
+    ord_rows_sorted = []
+
+    rows_sorted.each do |letter|
+      ord_rows_sorted << letter.ord
+    end
+
+rows_sorted_ctally = ord_rows_sorted[0]
+
+ord_rows_sorted.all? do |num|
+  num + 1 == rows_sorted_ctally += 1
+end
+
+
+
+    # if rows_sorted.length == 1
+    #     rows_good = true
+    # elsif
+    #     rows_sorted == rows_range_array
+    #     rows_good = true
+    # else
+    #     rows_good = false
+    # end
+
+    columns_sorted = @columns.sort.uniq
       x = columns_sorted[0]
       y = columns_sorted[-1]
     column_range_array = (x..y).to_a
 
-     rows_sorted = @rows.sort
-      a = rows_sorted[0]
-      b = rows_sorted[-1]
-    rows_range_array = (a..b).to_a
-binding.pry
-    return false if columns_sorted != column_range_array &&
-                    rows_sorted != rows_range_array
-    return false if columns_sorted == column_range_array &&
-                    rows_sorted == rows_range_array
-    if rows_sorted.uniq != 1 && rows_sorted == rows_range_array
+columns_sorted_ctally = columns_sorted[0]
 
-    columns_sorted.uniq != 1 && columns_sorted == column_range_array
+        columns_sorted.all? do |num|
+          num + 1 == columns_sorted_ctally += 1
+        end
 
+    # if columns_sorted.length == 1
+    #     columns_good = true
+    #   elsif
+    #     columns_sorted == column_range_array
+    #     columns_good = true
+    #   else
+    #     columns_good = false
+    # end
+    #
+    # return false if rows_good != true || columns_good != true
   end
-
-
-
-
-
-
-                    # binding.pry
-    # return true if  columns_sorted == column_range_array ||
-    #                 rows_sorted == rows_range_array
-        # binding.pry
 
 
 
   def no_diagonals
-    # binding.pry
-    @rows.uniq.length == 1 ||@columns.uniq.length == 1
+    if @rows.uniq.length == 1
+      @columns.uniq.length == 1
   end
+
+
+end
+# return false if columns_sorted != column_range_array &&
+#                 rows_sorted != rows_range_array
+# return false if columns_sorted == column_range_array &&
+#                 rows_sorted == rows_range_array
+# if rows_sorted.uniq != 1 && rows_sorted == rows_range_array
+#
+# # columns_sorted.uniq != 1 && columns_sorted == column_range_array
